f4fd515fb477e3b11467c050304984082e3cf89c
Conner Shumway <ckshum16@stlawu.edu>
2020-02-20 15:38:26 -0500

    assignment 2 schema

---
+++ schema.js
@@ -0,0 +1,20 @@
+// Define a plan for a collection
+
+const mongoose = require('mongoose');
+
+// Schema for a collection of voters
+const Voter = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  id: Number,
+  votes: [String]
+});
+
+// Speed up queries on all fields
+Voter.index({firstName: 1});
+Voter.index({lastName: 1});
+Voter.index({id: 1});
+Voter.index({votes: 1});
+
+// Compile and export this Schema
+module.exports = mongoose.model('Voter', Voter);
