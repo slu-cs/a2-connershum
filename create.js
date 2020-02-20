3200b2e60e8da876ccd74304d0f44830b86388f4
Conner Shumway <ckshum16@stlawu.edu>
2020-02-20 16:28:41 -0500

    create test

---
+++ create.js
@@ -0,0 +1,24 @@
+// Store some data in the faculty database
+
+const mongoose = require('mongoose');
+const connect = require('./db');
+const Professor = require('./schema');
+
+connect(); // To the database
+
+// File configurations
+const file = readline.createInterface({
+  input: fs.createReadStream('voters.csv')
+});
+
+// Asyncronous line-by-line input
+file.on('line', function(line) {
+  console.log(line);
+});
+
+// End the program when the file closes
+file.on('close', function() {
+  process.exit(0);
+});
+
+// Create the voters
