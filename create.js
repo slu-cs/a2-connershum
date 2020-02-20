9d3dea6abdbff54cde31ebb9365ba530970086f0
Conner Shumway <ckshum16@stlawu.edu>
2020-02-20 18:46:01 -0500

    create

--- create.js
+++ create.js
@@ -8,30 +8,35 @@ const Voter = require('./schema');
 
 connect(); // To the database
 
+// Create the voters
 const voters = [];
 
 // File configurations
 const filename = 'voters.csv';
 const file = readline.createInterface({
   input: fs.createReadStream(filename)
+
+
+  // Asyncronous line-by-line input
+  file.on('line', function(line) {
+    var array = line.split(',');
+
+    voters.push(new Voter({
+      firstName: array[0],
+      lastName: array[1],
+      id: array[2],
+      votes: array[3]
+    }));
+  });
+
+  // End the program when the file closes
+  file.on('close', function() {
+    process.exit(0);
+  });
 });
 
-// Asyncronous line-by-line input
-file.on('line', function(line) {
-  var array = line.split(',');
 
-  voters.push(new Voter({
-    firstName: array[0],
-    lastName: array[1],
-    id: array[2],
-    votes: array[3]
-  }));
-});
-
-// End the program when the file closes
-file.on('close', function() {
-  process.exit(0);
-});
-
-
-// Create the voters
+const saves = voters.map(v => v.save());
+Promise.all(saves)
+  .then(() => console.log('All saved'))
+  .catch(error => console.error(error.stack));
