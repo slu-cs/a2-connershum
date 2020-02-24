199339ec66936c8eb2581ee66fc14b3941e41f32
Conner Shumway <ckshum16@stlawu.edu>
2020-02-23 19:36:16 -0500

    ts

--- query.js
+++ query.js
@@ -22,6 +22,6 @@ const queries = [
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log('Voters in Canton: ', results[0].map(p => p.firstName));
+    console.log('Voters in Canton: ', results[0]);
 
   }).catch(error => console.error(error.stack));
